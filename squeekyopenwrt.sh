78575df5b4dcbad8bd69190bd579d6a14ca10ea6
squeekymouse89 <41025558+squeekymouse89@users.noreply.github.com>
2021-10-16 20:11:27 +0100

    Update squeekyopenwrt.sh
    issues with self delete

--- squeekyopenwrt.sh
+++ squeekyopenwrt.sh
@@ -3,12 +3,11 @@ echo https://images.linuxcontainers.org/images/openwrt/
 read -p 'Full URL: ' url
 read -p 'Container ID Number: ' number
 read -p 'Container Name: ' name
-wget $url /tmp/squeekysopenwrtrootfs.tar.xz
-pct create $number /tmp/rootfs.tar.xz --ostype unmanaged --hostname $name -net0 name=eth0 -net1 name=eth1
+wget $url -O /tmp/squeekysopenwrtrootfs.tar.xz
+pct create $number /tmp/squeekysopenwrtrootfs.tar.xz --ostype unmanaged --hostname $name -net0 name=eth0 -net1 name=eth1
 rm /tmp/squeekysopenwrtrootfs.tar.xz
 clear
 echo You can now find $name in the Proxmox Web UI and finish configuration. Please ensure you check your cpu, ram and nic settings. Two NICS have been created but not assigned to any interfaces. Enjoy !
 rm squeekyopenwrt.sh
-
 
 
